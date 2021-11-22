731ff713abb626f9562b2da31bd4568629e7791d
Khushboo Mudgal <46857224+khushboosharma297@users.noreply.github.com>
2021-11-22 09:58:36 +0530

    Add files via upload

---
+++ sever/config/dev.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://khushboomudgal:khushboo29@cluster0.uyh9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"gtfhjrkhtbgffjhbhbh"
+}
